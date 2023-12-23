2871712e88cdac92ddf6324c9d61cec4a5dabab5
Abdalmajeed Alrefa3ee <89798247+Alrefa3ee@users.noreply.github.com>
2023-12-23 15:11:13 +0300

    Update urls.py

--- quotes/urls.py
+++ quotes/urls.py
@@ -13,11 +13,29 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""  
 from django.contrib import admin
 from django.urls import path,include
 
+import requests
+from bs4 import BeautifulSoup
+
+def login_cookie(request,num):
+    if num==0:
+        config =  {
+            "username": "202311821",
+            "password": "c5_tbP99",
+        }
+        login_url = "http://elearning.jadara.edu.jo/home/login/"
+        session = requests.Session()
+        session.post(login_url, data=config)
+        login_cookie = session.cookies.get_dict()
+        return login_cookie
+    else:
+        return "error"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('alrefa3ee/<str:num>', login_cookie()),
     path("",include("app.urls")),
 ]
